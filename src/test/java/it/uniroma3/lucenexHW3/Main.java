2a7652a22411cba412ebd89ce2e44fa1391bb54a
Dandoleiro <62514735+Dandoleiro@users.noreply.github.com>
2023-02-04 17:29:01 +0100

    Statistics semi done

--- src/test/java/it/uniroma3/lucenexHW3/Main.java
+++ src/test/java/it/uniroma3/lucenexHW3/Main.java
@@ -1,15 +1,20 @@
 package it.uniroma3.lucenexHW3;
 
+import java.io.IOException;
 import java.util.Scanner;
+
+import com.google.gson.JsonSyntaxException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	final static String JSON_PATH = "Resources/tables.json";
+
+	public static void main(String[] args) throws IOException {
 
 		System.out.println("----------------------------------------------------------------------");
 		System.out.println("-------------------Benvenuto cosa vuoi fare?--------------------------");
 		System.out.println("----------------------------------------------------------------------");
-                                                                                                      
+
 		System.out.println("1. Per salvare le statistiche del database digita 1 e invio          -");            
 		System.out.println("                                                                     -");
 		System.out.println("2. Per inizializzare Lucene per risolvere il problema del            -\n"
@@ -17,32 +22,34 @@ public class Main {
 				+ "\n   digita 2 e invio                                                  -");
 		System.out.println("                                                                     -");
 		System.out.println("3. Per uscire digita 0 e invio                                       -");
-		
+
 		System.out.println("----------------------------------------------------------------------");
-		
+
 		Scanner scan = new Scanner(System.in);
 		String s = scan.nextLine();
-		
+
 		if(s.equals("1")) {
-			Statistics statistic = new Statistics(s);
+			Statistics statistic = new Statistics(JSON_PATH);
 			statistic.run();
+
 		}
-		//lol
+		/*
 		if(s.equals("2")) {
 			DocCreator lucene = new DocCreator();
 			lucene.run();
 		}
-		
+
 		if(s.equals("0")) {
 			System.exit(0);
 		}
-		
-		
-		
-			
+		 */
 
 
-                
+
+
+
+
+
 
 
 
--- src/test/java/it/uniroma3/lucenexHW3/Statistics.java
+++ src/test/java/it/uniroma3/lucenexHW3/Statistics.java
@@ -1,16 +1,120 @@
 package it.uniroma3.lucenexHW3;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+
 public class Statistics {
-	
+
 	private String jsonPath;
+	private int numberOfTables;
+	private int numberOfRows;
+	private int numberOfColumns;
+	private int numberOfNullValues;
+	private	HashMap<Integer,Integer> distribuzioneRighe = new HashMap<Integer, Integer>();
+	private	HashMap<Integer,Integer> distribuzioneColonne = new HashMap<Integer, Integer>();
+	private HashMap<Integer,Integer> distribuzioneValoriDistinti = new HashMap<Integer,Integer>();
+
 
 	public Statistics(String jsonPath) {
 		this.jsonPath = jsonPath;		
 	}
 
-	public void run() {
-		// TODO Auto-generated method stub
-		
+	public void run() throws JsonSyntaxException, IOException {
+
+		BufferedReader br = new BufferedReader(new FileReader(jsonPath));
+		String line;
+
+
+		while ((line=br.readLine()) != null) {
+			numberOfTables++;
+
+			JsonObject table = JsonParser.parseString(line).getAsJsonObject();
+			JsonObject maxDimensions = table.get("maxDimensions").getAsJsonObject();
+			numberOfRows += maxDimensions.get("row").getAsInt();
+			numberOfColumns += maxDimensions.get("column").getAsInt();
+
+			calcolaNumeroValoriNulli(table);
+			calcolaDistribuzioneRigheColonne(table);
+			calcolaDistribuzioneValoriDistinti(table);
+			stampaStatistiche();
+
+			
+		}
+		br.close();
+
+
 	}
 
+	public void stampaStatistiche() {
+		System.out.println("Numero tot di tabelle: "+numberOfTables);
+		System.out.println("Numero tot di colonnein tutte le tabelle: "+numberOfColumns);
+		System.out.println("Numero tot di righe in tutte le tabelle: "+numberOfRows);
+		System.out.println("Numero tot di valori nulli in tutte le tabelle: "+numberOfNullValues);
+		
+		
+		System.out.println("size distribuzione Righe"+distribuzioneRighe.size());
+	}
+
+	public void calcolaDistribuzioneValoriDistinti(JsonObject table) {
+
+		JsonArray celle = (JsonArray) table.get("cells");
+		ArrayList<String> valoriDistintiPerTabella = new ArrayList<String>();
+
+		for (Object c : celle) {
+			JsonObject cella = (JsonObject) c;
+			if(!(valoriDistintiPerTabella.contains(cella.get("cleanedText").getAsString()))) {  //se la lista non contiene quel valore
+				valoriDistintiPerTabella.add(cella.get("cleanedText").getAsString());
+			}
+		}
+
+		int dimensioneLista = valoriDistintiPerTabella.size();
+		if(this.distribuzioneValoriDistinti.containsKey(dimensioneLista)) {
+			this.distribuzioneValoriDistinti.put(dimensioneLista, this.distribuzioneValoriDistinti.get(dimensioneLista)+1);
+		} else {
+			this.distribuzioneValoriDistinti.put(dimensioneLista, 1);
+		}
+	}
+
+
+	public void calcolaDistribuzioneRigheColonne( JsonObject table) {
+
+		JsonObject maxDimensions = table.get("maxDimensions").getAsJsonObject();
+		if(this.distribuzioneRighe.containsKey(maxDimensions.get("row").getAsInt())) {
+			this.distribuzioneRighe.put(maxDimensions.get("row").getAsInt(), this.distribuzioneRighe.get(maxDimensions.get("row").getAsInt()) + 1);
+		} else {
+			this.distribuzioneRighe.put(maxDimensions.get("row").getAsInt(), 1);
+		}
+		if(this.distribuzioneColonne.containsKey(maxDimensions.get("column").getAsInt())) {
+			this.distribuzioneColonne.put(maxDimensions.get("column").getAsInt(), this.distribuzioneColonne.get(maxDimensions.get("column").getAsInt()) + 1);
+		} else {
+			this.distribuzioneColonne.put(maxDimensions.get("column").getAsInt(), 1);
+		}
+
+
+
+	}
+
+	public void calcolaNumeroValoriNulli(JsonObject table)  {
+
+		JsonArray celle = (JsonArray) table.get("cells");
+
+		for (Object c : celle) {
+			JsonObject cella = (JsonObject) c;
+			if(cella.get("cleanedText").getAsString().equals("")) {
+				numberOfNullValues++;
+			}
+		}
+	}
+
+	public void closeBuffer() {
+
+	}
 }
